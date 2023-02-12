4e7560a563851f14e54b1ac9545d620062df1ded
beom-yun <scds0326@naver.com>
2023-02-13 00:46:38 +0900

    #2.3 'As' and Attrs

--- src/App.js
+++ src/App.js
@@ -4,22 +4,18 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-  background-color: ${props => props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
-
-const Circle = styled(Box)`
-  border-radius: 50px;
+const Input = styled.input.attrs({ required: true })`
+  background-color: tomato;
 `;
 
 function App() {
   return (
-    <Father>
-      <Box bgColor="teal" />
-      <Box bgColor="tomato" />
-      <Circle bgColor="whitesmoke" />
+    <Father as="header">
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
     </Father>
   );
 }
