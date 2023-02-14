c8df9570bf1742cf6f40137e952f70e6c62e1bf6
beom-yun <58946778+beom-yun@users.noreply.github.com>
2023-02-14 23:32:39 +0900

    #5.2 Home part One

--- src/theme.ts
+++ src/theme.ts
@@ -1,3 +1,3 @@
 import { DefaultTheme } from 'styled-components';
 
-export const theme: DefaultTheme = { bgColor: '#2f3640', textColor: '#f5f6fa', accentColor: '#4cd137' };
+export const theme: DefaultTheme = { bgColor: '#2f3640', textColor: '#f5f6fa', accentColor: '#9c88ff' };
