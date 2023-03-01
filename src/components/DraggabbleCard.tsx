6f8461c5620d42b739e38cbc7e36ebf40fe5a835
beom-yun <scds0326@naver.com>
2023-03-01 11:56:28 +0900

    #7.12 Final Styles

--- src/components/DraggabbleCard.tsx
+++ src/components/DraggabbleCard.tsx
@@ -7,18 +7,25 @@ interface IDragabbleCardProps {
   index: number;
 }
 
-const Card = styled.div`
+const Card = styled.div<{ isDragging: boolean }>`
   background-color: ${props => props.theme.cardColor};
   padding: 10px 10px;
   border-radius: 5px;
   margin-bottom: 5px;
+  background-color: ${props => (props.isDragging ? '#74b9ff' : props.theme.cardColor)};
+  box-shadow: ${props => (props.isDragging ? '0px 2px 5px rgba(0, 0, 0, 0.05)' : 'none')};
 `;
 
 function DraggabbleCard({ toDo, index }: IDragabbleCardProps) {
   return (
     <Draggable draggableId={toDo} index={index}>
-      {magic => (
-        <Card ref={magic.innerRef} {...magic.dragHandleProps} {...magic.draggableProps}>
+      {(magic, snapshot) => (
+        <Card
+          isDragging={snapshot.isDragging}
+          ref={magic.innerRef}
+          {...magic.dragHandleProps}
+          {...magic.draggableProps}
+        >
           {toDo}
         </Card>
       )}
