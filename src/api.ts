4958b5c8e7ca8863a24a6ecc0a4fcdd84b60a91c
beom-yun <scds0326@naver.com>
2023-03-14 23:29:50 +0900

    #9.6 Home Screen part Two

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,25 @@
 const API_KEY = '054bf0b01347b0d4ed3c19516815888d';
 const BASE_PATH = 'https://api.themoviedb.org/3/';
 
+interface IMovie {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+
+export interface IGetMoviesResult {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: IMovie[];
+  total_pages: number;
+  total_results: number;
+}
+
 export function getMovies() {
   return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=kr`).then(response =>
     response.json()
