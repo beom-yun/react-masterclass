aea69520be522d308fc698fe23d86a68acccf7c6
beom-yun <58946778+beom-yun@users.noreply.github.com>
2023-03-15 19:26:47 +0900

    #9.8 Slider part Two

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -44,28 +44,43 @@ const Slider = styled.div`
 const Row = styled(motion.div)`
   display: grid;
   grid-template-columns: repeat(6, 1fr);
-  gap: 10px;
+  gap: 5px;
   position: absolute;
   width: 100%;
 `;
 
-const Box = styled(motion.div)`
+const Box = styled(motion.div)<{ bgPhoto: string }>`
   background-color: white;
+  background-image: url(${props => props.bgPhoto});
+  background-size: cover;
+  background-position: center center;
   height: 200px;
   color: red;
   font-size: 66px;
 `;
 
 const rowVariants: Variants = {
-  hidden: { x: window.outerWidth },
+  hidden: { x: window.outerWidth + 5 },
   visible: { x: 0 },
-  exit: { x: -window.outerWidth },
+  exit: { x: -window.outerWidth - 5 },
 };
+
+const offset = 6;
 
 function Home() {
   const { data, isLoading } = useQuery<IGetMoviesResult>(['movies', 'nowPlaying'], getMovies);
   const [index, setIndex] = useState(0);
-  const increaseIndex = () => setIndex(prev => prev + 1);
+  const [leaving, setLeaving] = useState(false);
+  const increaseIndex = () => {
+    if (data) {
+      if (leaving) return;
+      toggleLeaving();
+      const totalMovies = data.results.length - 1;
+      const maxIndex = Math.floor(totalMovies / offset) - 1;
+      setIndex(prev => (prev === maxIndex ? 0 : prev + 1));
+    }
+  };
+  const toggleLeaving = () => setLeaving(prev => !prev);
 
   return (
     <Wrapper>
@@ -78,7 +93,7 @@ function Home() {
             <Overview>{data?.results[0].overview}</Overview>
           </Banner>
           <Slider>
-            <AnimatePresence>
+            <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
               <Row
                 variants={rowVariants}
                 initial="hidden"
@@ -87,9 +102,12 @@ function Home() {
                 transition={{ type: 'tween', duration: 1 }}
                 key={index}
               >
-                {[1, 2, 3, 4, 5, 6].map(i => (
-                  <Box key={i}>{i}</Box>
-                ))}
+                {data?.results
+                  .slice(1)
+                  .slice(offset * index, offset * (index + 1))
+                  .map(movie => (
+                    <Box key={movie.id} bgPhoto={makeImagePath(movie.backdrop_path, 'w500')} />
+                  ))}
               </Row>
             </AnimatePresence>
           </Slider>
